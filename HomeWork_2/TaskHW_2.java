41a3fef45e24fb0396be99322d4cee473fbfa085
Vladimir <grandtwix33@mail.ru>
2023-03-12 18:39:12 +0500

    add task_3

--- HomeWork_2/TaskHW_2.java
+++ HomeWork_2/TaskHW_2.java
@@ -16,20 +16,23 @@ package HomeWork_2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class TaskHW_2 {
-    
-    public static void main(String args[]){
-        try{
-            BufferedReader file = new BufferedReader(new FileReader("test.txt"));
-            String temp;
-            while ((temp = file.readLine()) != null) {
-                System.out.println(temp);
-                // Как распарсить строку через StringBuilder так и не понял
-            }
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        BufferedReader file = new BufferedReader(new FileReader("test.txt"));
+        String temp;
+        while ((temp = file.readLine()) != null) {
+            String line1 = temp.replace('"', ' ');
+            StringBuilder result = new StringBuilder("");
+            String [] arrayData = line1.split(",");
+            String [] listName = {"Студент ", " получил ", " по предмету "};
+            for (int i =0; i < arrayData.length; i++) {
+                String[] arrData = arrayData[i].split(":");
+                result.append(listName[i]);
+                result.append(arrData[1]);
+                }
+            System.out.println(result);
         }
     }
 }
